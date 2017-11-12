c98802136ad6255d926a414f4169c7ffbda8b2cd
L-A-Davis <lindsay.amanda.davis@gmail.com>
2017-11-12 18:49:21 -0500

    completed bachelor methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,47 @@
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+    winner = data[season].find {|person| person["status"] == "Winner" }
+    winner["name"].split.first
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+    data.each do |season, contestants|
+        contestants.each do |person|
+            if person["occupation"] == occupation
+             return person["name"]
+        end
+       end
+    end
 end
-
+    
+    
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+      contestants.each do |person|
+          if person["hometown"] == hometown
+              counter +=1
+        end
+          end
+      end
+      counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season, contestants|
+       contestants.each do |person|
+           if person["hometown"] == hometown
+           return person["occupation"]
+        end
+      end
+   end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0
+  data[season].each do |person|
+      total += person["age"].to_f
+      end
+  (total / data[season].length).round
 end
