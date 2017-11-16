1e1ea39ab3f0cc0caf38b9fa62a19bd9eb1a060f
Marvin Clerge <clergemarvin@gmail.com>
2017-11-16 16:28:17 +0000

    Done

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each_with_index do |slot, index| #{"name" => "Ashley Iaconetti", etc}, 0
+    slot.each do |key, value| #"name", "Ashley Iaconetti"
+      if value == "Winner"
+        name = data[season][index]["name"]
+        return name.split()[0]
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array_h| #"season 19", array with hashes contianing contestants
+    array_h.each_with_index do |slot, index| #{"name" => "Ashley Iaconetti", etc}, 0
+      slot.each do |key, value| #"name", "Ashley Iaconetti"
+        return data[season][index]["name"] if value == occupation
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.each do |season, array_h| #"season 19", array with hashes contianing contestants
+    array_h.each_with_index do |slot, index| #{"name" => "Ashley Iaconetti", etc}, 0
+      slot.each do |key, value| #"name", "Ashley Iaconetti"
+        if value == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array_h| #"season 19", array with hashes contianing contestants
+    array_h.each_with_index do |slot, index| #{"name" => "Ashley Iaconetti", etc}, 0
+      slot.each do |key, value| #"name", "Ashley Iaconetti"
+        return data[season][index]["occupation"] if value == hometown
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+
+  data[season].each_with_index do |slot,index| #{"name" => "Ashley Iaconetti", etc}, 0
+    slot.each do |key, value| #"name", "Ashley Iaconetti"
+      ages << data[season][index]["age"].to_i
+    end
+  end
+
+  (ages.reduce{|sum, i| sum + i}.to_f / ages.size).round
 end
